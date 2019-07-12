ec04a967a945f0baf68c49f2396c135e49f2f7e5
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-07-12 09:05:39 +0545

    add async error handler

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 const router = require("./router");
 const dbConnection = require("./config/db");
 require("dotenv").config();
+require("express-async-errors");
 
 dbConnection();
 
@@ -11,6 +12,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/", router);
+
+// Error handling middleware
+app.use(function(err, req, res, next) {
+ console.log(err);
+ return res.status(500).json({
+   error: errorHandler(err) || "Something went wrong!"
+ });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running at ${PORT}`);
